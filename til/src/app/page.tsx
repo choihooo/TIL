55993277c300dee920717e6121a6c66639655b47
choihooo <hochoi8621@naver.com>
2024-11-05 02:11:50 +0900

    <style>: <tag 부분 퍼블리싱>
    - tag가 들어가는 부분을 만듦
    
    Resolved: #1

--- til/src/app/page.tsx
+++ til/src/app/page.tsx
@@ -1,10 +1,13 @@
-import StampCalendar from "./components/StampCalendar";
 import styles from "./Home.module.scss";
+import StampCalendar from "./components/StampCalendar";
+import AllTag from "./components/AllTag";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.column}>난 태그</div>
+      <div className={styles.column}>
+        <AllTag></AllTag>
+      </div>
       <div className={styles.column}>
         <StampCalendar />
       </div>
