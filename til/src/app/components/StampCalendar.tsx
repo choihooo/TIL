0098a798ebe538f2373a9d8566237ea23561dcf3
choihooo <hochoi8621@naver.com>
2024-11-05 00:09:19 +0900

    <style>: <메인 레이아웃 설정 및 스탬프판 구현>
    - 메인 레이아웃 설정
    - 스탬프판 구현
    
    Resolved: #1

--- til/src/app/components/StampCalendar.tsx
+++ til/src/app/components/StampCalendar.tsx
@@ -1,71 +1,102 @@
 "use client";
 
 import React, { useState } from "react";
+import Image from "next/image";
 import dayjs, { Dayjs } from "dayjs";
 import styles from "./StampCalendar.module.scss";
 
 const StampCalendar: React.FC = () => {
   const [currentDate, setCurrentDate] = useState<Dayjs>(dayjs());
 
-  // 해당 월의 날짜들을 배열로 반환
   const getCalendarDays = () => {
     const startOfMonth = currentDate.startOf("month");
     const endOfMonth = currentDate.endOf("month");
     const daysInMonth: Dayjs[] = [];
 
-    // 첫 주의 시작을 맞추기 위해 이전 달 며칠을 포함
     let day = startOfMonth.startOf("week");
-
     while (day <= endOfMonth.endOf("week")) {
       daysInMonth.push(day);
       day = day.add(1, "day");
     }
-
     return daysInMonth;
   };
 
-  // 달 이동 핸들러
   const handleMonthChange = (months: number) => {
     setCurrentDate((prevDate) => prevDate.add(months, "month"));
   };
 
   return (
-    <div className={styles.calendar}>
-      <div className={styles.calendarHeader}>
-        <button type="button" onClick={() => handleMonthChange(-12)}>
-          {"<<"}
-        </button>
-        <button type="button" onClick={() => handleMonthChange(-1)}>
-          {"<"}
-        </button>
-        <span>{currentDate.format("YYYY년 MM월")}</span>
-        <button type="button" onClick={() => handleMonthChange(1)}>
-          {">"}
-        </button>
-        <button type="button" onClick={() => handleMonthChange(12)}>
-          {">>"}
-        </button>
-      </div>
-
-      <div className={styles.calendarGrid}>
-        {["일", "월", "화", "수", "목", "금", "토"].map((day, index) => (
-          <div key={index} className={styles.calendarDayName}>
-            {day}
-          </div>
-        ))}
-        {getCalendarDays().map((day, index) => (
-          <div
-            key={index}
-            className={`${styles.calendarDay} ${
-              day.isSame(currentDate, "month")
-                ? styles.currentMonth
-                : styles.otherMonth
-            }`}
+    <div className={styles["calendar__container"]}>
+      <div>
+        <div className={styles["calendar__header"]}>
+          <button
+            className={styles["calendar__button"]}
+            type="button"
+            onClick={() => handleMonthChange(-12)}
           >
-            <div className={styles.date}>{day.date()}</div>
-            <div className={styles.stamp}>도장</div>
+            {"<<"}
+          </button>
+          <button
+            className={styles["calendar__button"]}
+            type="button"
+            onClick={() => handleMonthChange(-1)}
+          >
+            {"<"}
+          </button>
+          <span className={styles["calendar__year-month"]}>
+            {currentDate.format("YYYY년 MM월")}
+          </span>
+          <button
+            className={styles["calendar__button"]}
+            type="button"
+            onClick={() => handleMonthChange(1)}
+          >
+            {">"}
+          </button>
+          <button
+            className={styles["calendar__button"]}
+            type="button"
+            onClick={() => handleMonthChange(12)}
+          >
+            {">>"}
+          </button>
+        </div>
+        <div className={styles["calendar__grid-wrapper"]}>
+          <div className={styles["calendar__grid"]}>
+            {["일", "월", "화", "수", "목", "금", "토"].map((day, index) => (
+              <div key={index} className={styles["calendar__day-name"]}>
+                {day}
+              </div>
+            ))}
+            {getCalendarDays().map((day, index) => (
+              <div
+                key={index}
+                className={`${styles["calendar__day-cell"]} ${
+                  day.isSame(currentDate, "month")
+                    ? styles["calendar__day-cell--current-month"]
+                    : styles["calendar__day-cell--other-month"]
+                }`}
+              >
+                <div className={styles["calendar__date"]}>{day.date()}</div>
+                <div className={styles["calendar__stamp"]}>
+                  <Image src="/stamp.svg" alt="스탬프" width={30} height={30} />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+
+          <div className={styles["calendar__right-column"]}>
+            <div>주차 정보</div>
+            {["1주", "2주", "3주", "4주", "이번달"].map((label, index) => (
+              <div key={index} className={styles["calendar__day-cell"]}>
+                <div className={styles["calendar__date"]}>{label}</div>
+                <div className={styles["calendar__stamp"]}>
+                  <Image src="/stamp.svg" alt="스탬프" width={30} height={30} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- til/src/app/page.tsx
+++ til/src/app/page.tsx
@@ -1,5 +1,14 @@
 import StampCalendar from "./components/StampCalendar";
+import styles from "./Home.module.scss";
 
 export default function Home() {
-  return <StampCalendar />;
+  return (
+    <div className={styles.container}>
+      <div className={styles.column}>난 태그</div>
+      <div className={styles.column}>
+        <StampCalendar />
+      </div>
+      <div className={styles.column}>난 검색</div>
+    </div>
+  );
 }
