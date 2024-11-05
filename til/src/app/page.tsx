ead046c7b12861a7bb99c2c2e857271cb096a2a6
choihooo <hochoi8621@naver.com>
2024-11-05 11:18:36 +0900

    <style>: <검색 부분 퍼블리싱>
    - 검색 들어가는 부분을 만듦
    
    Resolved: #1

--- til/src/app/page.tsx
+++ til/src/app/page.tsx
@@ -2,6 +2,7 @@ import styles from "./Home.module.scss";
 import StampCalendar from "./components/StampCalendar";
 import AllTag from "./components/AllTag";
 import Content from "./components/Content";
+import Search from "./components/Search";
 
 export default function Home() {
   return (
@@ -13,7 +14,9 @@ export default function Home() {
         <StampCalendar />
         <Content />
       </div>
-      <div className={styles.column}>난 검색</div>
+      <div className={styles.column}>
+        <Search />
+      </div>
     </div>
   );
 }
