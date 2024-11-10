13acd87f95d9fc2d47a5684060dbb2fdfb8aaaf8
howu <67588757+choihooo@users.noreply.github.com>
2024-11-11 00:27:18 +0900

    Hotfix/post server error (#7)
    * chore: 이슈 & 커밋 & PR 템플릿 설정
    
    - 프로젝트를 진행에 나갈때 사용할 템플릿 설정
    
    Resolved: 간단한 작업이라 따로 이슈 X
    
    * chore: PR 템플릿 설정
    - PR 템플릿 재 업로드
    
    Resolved: 간단한 작업이라 따로 이슈 X
    
    * [style] 메인 페이지 퍼블리싱 완료 (#3) (#4)
    
    * chore: 이슈 & 커밋 & PR 템플릿 설정
    
    - 프로젝트를 진행에 나갈때 사용할 템플릿 설정
    
    Resolved: 간단한 작업이라 따로 이슈 X
    
    * chore: PR 템플릿 설정
    - PR 템플릿 재 업로드
    
    Resolved: 간단한 작업이라 따로 이슈 X
    
    * <chore>: <config 파일 설정 및 getConfig 함수 생성> (#2)
    
    - navBar를 만들때 사용자의 이름을 로고로 띄워주기 위해 사용자 이름을 받을 config 파일을 만들었다
    - 또한 나중에 사용할 수도 있는 정보들을 미리 받을 수 있게 다른 정보들도 받는다
    
    * <style>: <scss 설정 및 캘린더 ui 구현>
    
    - scss 적용
    - 캘린더 ui 구현
    
    Resolved: #1
    
    * <style>: <메인 레이아웃 설정 및 스탬프판 구현>
    
    - 메인 레이아웃 설정
    - 스탬프판 구현
    
    Resolved: #1
    
    * <style>: <빌드 오류 해결>
    
    - NavBar 컴포넌트에 ' 부분 변경
    - HTML의 특수 문자가 의도치 않게 HTML 구문으로 해석되는 것을 방지
    
    Resolved: #1
    
    * <style>: <빌드 오류 수정>
    
    - 임포트할때 NavBar라고 해야하는 걸 Navbar로 오타
    
    Resolved: #1
    
    * <style>: <tag 부분 퍼블리싱>
    
    - tag가 들어가는 부분을 만듦
    
    Resolved: #1
    
    * <style>: <content 부분 퍼블리싱 및 태그 부분 수정>
    
    - content 부분 퍼블리싱
    - 자주 쓰는 폰트 사이즈 변수로 등록
    
    Resolved: #1
    
    * <style>: <검색 부분 퍼블리싱>
    
    - 검색 들어가는 부분을 만듦
    
    Resolved: #1
    
    * <feature>: <글 목록 컴포넌트와 글 상세 페이지 추가>
    - 글 목록 컴포넌트를 폴더구조와 연결해서 목록 페이지 완성
    - 글 상세 페이지를 생성하고 동적으로 라우팅 함
    Resolved: #5
    
    * <feature> : <build 오류 수정>
    
    * <hotfix> : <포스트 상세 페이지에서 일어나는 서버 오류 해결>
    
    * Rename MDXcontent.tsx to MDXContent.tsx

---
+++ til/src/app/[category]/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import fs from "fs/promises";
+import path from "path";
+import matter from "gray-matter";
+import { serialize } from "next-mdx-remote/serialize";
+import dayjs from "dayjs";
+import MDXContent from "./components/MDXContent";
+
+interface PostMatter {
+  title: string;
+  date: string;
+  category?: string;
+}
+
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ category: string; slug: string }>;
+}) {
+  const resolvedParams = await params; // Résolution de la promesse pour obtenir les valeurs
+
+  const { category, slug } = resolvedParams;
+
+  const filePath = path.join(process.cwd(), "posts", category, `${slug}.mdx`);
+
+  try {
+    const fileContents = await fs.readFile(filePath, "utf8");
+    const { data, content } = matter(fileContents);
+    const grayMatter = data as PostMatter;
+
+    const mdxSource = await serialize(content);
+    const dateString = dayjs(grayMatter.date)
+      .locale("ko")
+      .format("YYYY년 MM월 DD일");
+
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h1>{grayMatter.title}</h1>
+        <p>{dateString}</p>
+        <article>
+          <MDXContent source={mdxSource} />
+        </article>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error loading post:", error);
+  }
+}
