dfb156777d1316b21956fa8d0e30353938b99bb9
choihooo <hochoi8621@naver.com>
2024-11-19 03:25:19 +0900

    새로고침 오류 해결

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
       "@pages": path.resolve(__dirname, "./src/pages"),
     },
   },
+  base: "/", // 기본 경로. 배포 환경에서 프로젝트의 루트 경로 설정
+  server: {
+    port: 3000, // 개발 서버의 포트
+    open: true, // 개발 서버 실행 시 브라우저 자동 열기
+    historyApiFallback: true, // React Router를 사용하는 SPA에서 새로고침 시 경로 문제 해결
+  },
+  build: {
+    outDir: "dist", // 빌드 결과 디렉토리
+  },
 });
