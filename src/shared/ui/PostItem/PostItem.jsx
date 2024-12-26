e19ab3ad53fe8177f9da6f3b87fe885f36e12d29
choihooo <hochoi8621@naver.com>
2024-12-27 07:56:43 +0900

    [style] 포스트 아이템 스타일 변경

--- src/shared/ui/PostItem/PostItem.jsx
+++ src/shared/ui/PostItem/PostItem.jsx
@@ -10,19 +10,19 @@ function PostItem({ post }) {
   return (
     <>
       <Link to={`/post/${post.id}`} className="post">
-        <div className="post__content">
-          <div className="post__info">
-            <h2 className="post__title">{post.title}</h2>
-            <p className="post__date">{post.date}</p>
-            <p className="post__excerpt">{post.excerpt}</p>
-          </div>
-        </div>
+        <h2 className="post__title">{post.title}</h2>
         <div className="post__thumbnail-wrapper">
           <img
             src={post.thumbnail}
             alt={post.title}
             className="post__thumbnail"
           />
+        </div>{" "}
+        <div className="post__content">
+          <div className="post__info">
+            <p className="post__date">{post.date}</p>
+            <p className="post__excerpt">{post.excerpt}</p>
+          </div>
         </div>
       </Link>
     </>
