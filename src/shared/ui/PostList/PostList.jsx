3e20dec64115b129f5fdabc66448e6f58364ae7d
choihooo <hochoi8621@naver.com>
2024-12-28 22:25:17 +0900

    [hotfix] 블로그 글 볼때 3개만 보이는 것을 전부 다 보이게 수정

--- src/shared/ui/PostList/PostList.jsx
+++ src/shared/ui/PostList/PostList.jsx
@@ -5,8 +5,7 @@ import "./PostList.scss";
 function PostList({ posts }) {
   const sortedPosts = posts
     .slice()
-    .sort((a, b) => new Date(b.date) - new Date(a.date)) // 최신 날짜 기준 정렬
-    .slice(0, 3);
+    .sort((a, b) => new Date(b.date) - new Date(a.date)); // 최신 날짜 기준 정렬
 
   return (
     <div className="post-list">
