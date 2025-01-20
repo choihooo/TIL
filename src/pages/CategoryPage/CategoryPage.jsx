4c90a93d1fc614802bd5805dfc6f387d6e5761bc
choihooo <hochoi8621@naver.com>
2025-01-21 02:50:58 +0900

    [hotfix] 카테고리 페이지도 수정

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import PostList from "../../shared/ui/PostList/PostList";
 import { getPosts } from "../../utils/postService";
 import styles from "./CategoryPage.module.scss";
+import { Helmet } from "react-helmet-async";
 
 function CategoryPage() {
   const { main } = useParams();
@@ -18,6 +19,27 @@ function CategoryPage() {
 
   return (
     <div className={styles["category-page"]}>
+      <Helmet>
+        <title>Howu</title>
+        <meta
+          name="description"
+          content="최호의 코딩 블로그입니다! 최신 글을 만나보세요."
+        />
+        <meta property="og:title" content="Howu" />
+        <meta
+          property="og:description"
+          content="최호의 코딩 블로그입니다! 최신 글을 만나보세요."
+        />
+        <meta property="og:url" content="https://blog.howu.run" />
+        <meta
+          property="og:image"
+          content="https://blog.howu.run/images/banner.jpg"
+        />
+        <meta property="og:image:width" content="1200" /> // 적절한 이미지 크기
+        지정
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
       <h2 className={styles["category-page__title"]}>
         {main.toUpperCase()} 게시판
       </h2>
